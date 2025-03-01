23563238a891d7fa8bca2ab374968b18d175afcd
Avishukla-student <avishukla8303@gmail.com>
2025-03-01 11:16:18 +0530

    changing files

--- main.js
+++ main.js
@@ -5,14 +5,43 @@ const buttons=document.querySelectorAll(".button");
 const equel=document.querySelector(".equel");
 const ac=document.querySelector(".ac");
 const ascape=document.querySelector(".escape");
+const operat=document.querySelectorAll(".a");
+let clickcount=0;
 
 
 Array.from(buttons).forEach((button) =>{
     button.addEventListener("click",(evt)=>{
         string= string + evt.target.innerHTML;
         input.value= string;
+    });
+});
 
-    })
+operat.forEach((a)=>{
+    a.addEventListener("click",(evt)=>{
+        clickcount++;
+        if(clickcount===1){
+            string= string + evt.target.innerHTML;
+            input.value= string;
+
+        }else
+        {
+            let inpute=document.querySelector("input").value;
+            let match= inpute.match(/([-+]?\d*\.?\d+)\s*([\+\-\×\÷])\s*([-+]?\d*\.?\d+)/);
+            
+            if(match){
+                let a=parseFloat(match[1]);
+                let operator=match[2];
+                let b=parseFloat(match[3]);
+
+                
+                string=calculate();
+                string=string+evt.target.innerHTML;
+                input.value= string;
+               
+                clickcount=2;
+            }
+        }        
+    });
 });
 
 equel.addEventListener("click",()=>{
@@ -22,18 +51,20 @@ equel.addEventListener("click",()=>{
 
 ascape.addEventListener("click",()=>{
     string=input.value=input.value.slice(0,-1) ;
+    clickcount=0;
 });
 
 ac.addEventListener("click",()=>{
     string="";
     input.value= string;
     label.innerText= string;
+    clickcount=0;
     
 });
 
 function calculate(){
     let input=document.querySelector("input").value;
-    let match= input.match(/([-+]?\d*\.?\d+)\s*([\+\-\×\÷])\s*([-+]?\d*\.?\d+)/);
+    let match= input.match(/([-+]?\d*\.?\d+)\s*([\+\-\×\÷])\s*([-+]?\d*\.?\d+)/);   
     let result;
 
     if (match){
@@ -50,12 +81,10 @@ function calculate(){
             default:result="invali operator";
         }
 
-
         label.innerText= result;
     }
     else{
         label.innerText="invalid input format";
     }
-
-    string=result;
-}
+    return  result;    
+};
